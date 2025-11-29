69b480ddb99c98f615f64bdec000d8f67c2d6f9d
Hermit-commits-code <hotcupofjoe2013@gmail.com>
2025-11-28 23:21:39 -0500

    fix(ci): update widget test to use GalaxySentinelApp; remove unused import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:galaxy_sentinel/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const GalaxySentinelApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
